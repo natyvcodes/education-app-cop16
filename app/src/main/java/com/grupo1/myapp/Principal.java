f2a9de2e3df07c7cdc7123ff0ac2f58e99f1a07e
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2024-11-23 18:54:18 -0500

    Navigation bar added

--- app/src/main/java/com/grupo1/myapp/Principal.java
+++ app/src/main/java/com/grupo1/myapp/Principal.java
@@ -1,26 +1,18 @@
 package com.grupo1.myapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Principal extends AppCompatActivity {
-    DatabaseHelper databaseHelper = new DatabaseHelper(this);
-    private TextView articulo;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_principal);
-        //Este es una prueba
-        articulo = findViewById(R.id.parrafo);
-        databaseHelper.insertarArticulo(1,"La guerra ecologica", "Judy",12032024,"Un texto random", "no hay", "No hay");
-        String text = databaseHelper.mostrarParrafo(1);
-        articulo.setText(text);
+
     }
+
 }
