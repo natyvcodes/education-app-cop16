a62958bdf297180fe05159758756ec933e703879
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2024-11-23 11:39:40 -0500

    Database functionality added, methods to add info and a test

--- app/src/main/java/com/grupo1/myapp/DatabaseHelper.java
+++ app/src/main/java/com/grupo1/myapp/DatabaseHelper.java
@@ -1,24 +1,79 @@
 package com.grupo1.myapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE = "cop.db";
+    private static final int VERSION = 1;
 
-    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public static final String TABLE_USUARIOS = "HOLA";
+    public static final String TABLE_LECTURA = "HOLa";
+    public static final String TABLE_ARTICULO = "CREATE TABLE ARTICULO ("
+            + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + "nombre TEXT NOT NULL,"
+            + "autor TEXT NOT NULL,"
+            + "fecha INTEGER NOT NULL,"
+            + "parrafo TEXT NOT NULL,"
+            + "audio TEXT,"
+            + "imagen TEXT);";
+    public static final String TABLE_PLANTA = "Hola";
+    public static final String TABLE_ANIMAL = "";
+
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE, null, VERSION);
     }
 
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(TABLE_ARTICULO);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+    public void insertarArticulo(int id, String nombre, String autor, int fecha, String parrafo, String audio, String imagen){
+        SQLiteDatabase db = this.getReadableDatabase();
+        ContentValues valores = new ContentValues();
+
+        valores.put("id", id);
+        valores.put("nombre", nombre);
+        valores.put("autor", autor);
+        valores.put("fecha",fecha);
+        valores.put("parrafo", parrafo);
+        valores.put("audio", audio);
+        valores.put("imagen", imagen);
+
+        long resultado = db.insert("Articulo", null, valores);
+
+        if (resultado == -1) {
+            Log.e("DB_ERROR", "Error al insertar el articulo.");
+        } else {
+            Log.d("DB_SUCCESS", "Articulo insertado correctamente con ID: " + resultado);
+        }
+        db.close();
+    }
+    public String mostrarParrafo(int idArticulo){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String texto = "";
+
+        Cursor cursor = db.rawQuery("SELECT parrafo FROM articulo WHERE id = ?",
+                new String[]{String.valueOf(idArticulo)});
+
+        if (cursor.moveToFirst()) {
+            texto = cursor.getString(0); // Obtén el valor de la primera columna
+        }
+
+        cursor.close();
+        db.close();
+        return texto;
     }
 
 }
--- app/src/main/java/com/grupo1/myapp/Principal.java
+++ app/src/main/java/com/grupo1/myapp/Principal.java
@@ -1,6 +1,7 @@
 package com.grupo1.myapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +10,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Principal extends AppCompatActivity {
-
+    DatabaseHelper databaseHelper = new DatabaseHelper(this);
+    private TextView articulo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_principal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        //Este es una prueba
+        articulo = findViewById(R.id.parrafo);
+        databaseHelper.insertarArticulo(1,"La guerra ecologica", "Judy",12032024,"Un texto random", "no hay", "No hay");
+        String text = databaseHelper.mostrarParrafo(1);
+        articulo.setText(text);
     }
 }
