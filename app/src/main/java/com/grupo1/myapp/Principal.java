5ea77cb338390c6103a124cbbe0ed0f40ab69fe5
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2024-11-23 19:41:40 -0500

    Scroll fixed

--- app/src/main/java/com/grupo1/myapp/Principal.java
+++ app/src/main/java/com/grupo1/myapp/Principal.java
@@ -2,17 +2,24 @@ package com.grupo1.myapp;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Principal extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_principal);
-
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        int id = bottomNavigationView.getSelectedItemId();
+        Log.d("Hola", "El id" + id);
     }
 
 }
